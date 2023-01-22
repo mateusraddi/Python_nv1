4ce32fbc0f498398fcc72df69b5ba64313f38c7e
Mateus Räddi <mateusraddimendonca@gmail.com>
2023-01-22 14:59:35 -0300

    Create desafio 13.py

---
+++ desafio 13.py
@@ -0,0 +1,7 @@
+s= float(input('Qual é o salario do Funcionário? R$'))
+a = (s/10) / 2
+c1 = (s/10+s+a)
+print(f'Um funcionario que ganhava R${s}, com 15% de aumento, passa a receber R$ {c1:.2f}')
+
+
+
