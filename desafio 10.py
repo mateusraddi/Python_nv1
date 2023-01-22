c9114185b8d0de71fd506ee232a44bb5672b8637
Mateus Räddi <mateusraddimendonca@gmail.com>
2023-01-22 13:53:15 -0300

    Create desafio 10.py

---
+++ desafio 10.py
@@ -0,0 +1,8 @@
+d = int(input('digite um numero pra ver sua tabuada: '))
+print('='*12)
+print(f'{d} x  1 = {d*1}\n{d} x  2 = {d*2}\n{d} x  3 = {d*3}\n{d} x  4 = {d*4}\n{d} x  5 = {d*5}')
+print(f'{d} x  6 = {d*6}\n{d} x  7 = {d*7}\n{d} x  8 = {d*8}\n{d} x  9 = {d*9}\n{d} x 10 = {d*10}')
+print('='*12)
+
+
+
