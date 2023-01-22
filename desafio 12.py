f0a1d92702b744e5ecab0523d0e9d5d18667449f
Mateus Räddi <mateusraddimendonca@gmail.com>
2023-01-22 14:40:02 -0300

    Update desafio 12.py

--- desafio 12.py
+++ desafio 12.py
@@ -1,4 +1,7 @@
-n1 = int(input('Qual é o preço de um produto? R$'))
-n2 = (n1/10) / 2
+n1 = float(input('Qual é o preço de um produto? R$'))
+n2 = n1 - (n1/10)/2
 print(f'O produto que custava {n1}, na promocão de 5% vai custar R${n2:.2f}')
+
+
+
 
