5cfc7ee7d21ec22ae8754c2895f4c1f53ea32b2c
Mateus Räddi <mateusraddimendonca@gmail.com>
2023-01-23 18:56:16 -0300

    Update desafio 16.py

--- desafio 16.py
+++ desafio 16.py
@@ -1,5 +1,5 @@
 import math
-from match import floor
+
 n = float(input('Digite um valor: '))
 
-print(f'o valor  digitado  foi {n} e a sua porcão inteira é {s}')
+print(f'o valor  digitado  foi {n} e a sua porcão inteira é {math.floor(n)}')
