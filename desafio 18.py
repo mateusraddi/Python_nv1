edb700e55479f6bc1c72781e705c9327ea22c973
Mateus Räddi <mateusraddimendonca@gmail.com>
2023-01-23 19:42:18 -0300

    Create desafio 18.py

---
+++ desafio 18.py
@@ -0,0 +1,11 @@
+import math
+
+n = float(input('Digite o ângulo que você deseja: '))
+a = (math.radians(n))
+print(f'O ângulo de {n} tem o SENO de {math.sin(a):.2f}')
+print(f'O ângulo de {n} tem o COSSENO de {math.cos(a):.2f}')
+print(f'O ângulo de {n} tem o TANGENTE de {math.tan(a):.2f}')
+
+
+
+
