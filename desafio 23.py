b3551ee3708da79e201f43445191eecf8cc0cd1c
Mateus Räddi <mateusraddimendonca@gmail.com>
2023-01-26 23:02:22 -0300

    Create desafio 23.py

---
+++ desafio 23.py
@@ -0,0 +1,12 @@
+print('-'*22)
+n = int(input('Informe um numero: '))
+u = (n/1) % 10
+d = (n/10) % 10
+c= (n/100) % 10
+m = (n/1000) % 10
+input('Analizando o numero:...')
+print(f'Unidade: {u:.0f} \nDezena: {d:.0f} \nCentena: {c:.0f} \nMilhar: {m:.0f}')
+print('-'*22)
+
+
+
