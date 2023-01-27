934df4fa74fa15f07c6a0b6cda4b004ca181b6f2
Mateus Räddi <mateusraddimendonca@gmail.com>
2023-01-26 22:27:37 -0300

    Update desafio 22.py

--- desafio 22.py
+++ desafio 22.py
@@ -1,9 +1,14 @@
 nome = str(input('Digite seu nome completo: ')).strip()
+n = input('Digite seu primeiro nome: ')
 print('Analisando seu nome...')
 print(f'Seu nome em letras maiúsculas é {nome.upper()}')
 print(f'Seu nome em letras minúsculas é {nome.lower()}')
 print(f'Seu nome tem ao todo tem {len(nome)-nome.count(" ")} letras')
-print(f'Seu primeiro nome é {nome.split("0")} e ele tem {nome.find(" ")}')
+print(f'Seu primeiro nome é {n} e ele tem {nome.find(" ")} letras')
+
+
+
+
 
 
 
