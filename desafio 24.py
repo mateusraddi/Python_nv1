59798c6ee53eddccc7c05a32e74697efdead14a6
Mateus Räddi <mateusraddimendonca@gmail.com>
2023-01-28 21:03:00 -0300

    Update desafio 24.py

--- desafio 24.py
+++ desafio 24.py
@@ -1,12 +1,3 @@
-i
-
-#frase a ser corrigida
-frase_a_ser_corrigida = TextBlob("altinopolis")
 cidade = str(input('Em que cidade voçê nasceu? ')).strip()
-print(cidade.upper() == 'ALTINÓPOLIS')
-
-
-
-
-
+print(cidade[:11].upper() == 'ALTINOPOLIS')
 
