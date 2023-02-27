7b29974e461157bc42a8de975288e2753fe98325
Mateus Räddi <mateusraddimendonca@gmail.com>
2023-02-26 21:29:17 -0300

    Update desafio 28.py

--- desafio 28.py
+++ desafio 28.py
@@ -5,4 +5,4 @@ r = (multa*velocidade)/kilometros
 if velocidade <=80:
     print('Tenha um Bom Dia! Dirija com Segurança')
 else:
-        print(f'Multado! voce excedeu o limite permitido que é de 80km/h\nvoce deve pagar uma multa de de {r}')
+        print(f'Multado! Voce Excedeu o Limite Permitido que é de 80km/h\nVoce Deve Pagar uma Multa de de {r}')
