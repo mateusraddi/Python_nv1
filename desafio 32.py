8785e1d50ec8c0af7e158d3c08f4b8f2fbdb19fd
Mateus Räddi <123032481+mateusraddi@users.noreply.github.com>
2023-11-11 17:43:16 -0300

    Create desafio 32.py

---
+++ desafio 32.py
@@ -0,0 +1,9 @@
+a = int(input('digite um numero: '))
+b = 50
+if a > 50:
+    print(f'o numero {a} é maior que 50.')
+elif a == b:
+    print('o numero é igual a 50.')
+
+else:
+    print(f'o numero {a} é menor que 50.')
